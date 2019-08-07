bae8c09f7f9797be22a22cb6c9d1b1c3db4b422f
Serhii <davydko.serhiy@gmail.com>
2019-08-07 15:15:34 +0300

    fix

--- src/sources.js
+++ src/sources.js
@@ -1,7 +1,8 @@
 export const ADRESS = 'https://fmail.flyaps.com/api/v1/mailbox/messages/';
 
 export const getData = async(link) => {
-  const response = await fetch(link, { mode: 'no-cors'});
+
+  const response = await fetch(link);
   const posts = await response.json();
 
   return posts;

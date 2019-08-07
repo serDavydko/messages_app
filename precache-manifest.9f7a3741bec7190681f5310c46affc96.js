5ca9b242cf32e13555ca3a3d88fbdbef207aad95
Serhii <davydko.serhiy@gmail.com>
2019-08-07 12:43:30 +0300

    Updates

---
+++ precache-manifest.9f7a3741bec7190681f5310c46affc96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2eff32e61fea100e67e1f35d83d12f7",
+    "url": "/messages_app/index.html"
+  },
+  {
+    "revision": "4c252ce2795573b4ccd7",
+    "url": "/messages_app/static/css/main.6a2b93c8.chunk.css"
+  },
+  {
+    "revision": "a8736b539be561d566c2",
+    "url": "/messages_app/static/js/2.eeefd254.chunk.js"
+  },
+  {
+    "revision": "4c252ce2795573b4ccd7",
+    "url": "/messages_app/static/js/main.316c03f8.chunk.js"
+  },
+  {
+    "revision": "fd3a275ed60b06c7274d",
+    "url": "/messages_app/static/js/runtime~main.8a59438e.js"
+  }
+]);
